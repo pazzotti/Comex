bbafa5d12b722515cc8dbe7dee6657c5f2ecd4b6
pazzotti <pazzotti@live.com>
2023-06-11 11:46:16 -0300

    graficos iniciais

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,22 +3,28 @@ import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-chart',
-  template: `
-    <highcharts-chart
-      [Highcharts]="Highcharts"
-      [options]="chartOptions"
-      style="width: 100%; height: 400px;"
-    ></highcharts-chart>
-  `
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+
+
 })
 export class DashboardComponent {
+
+  dataInicial: Date = new Date();
+  dataFinal: Date = new Date();
+
+  constructor() {
+
+  }
   Highcharts: typeof Highcharts = Highcharts;
-  chartOptions: Highcharts.Options = {
+  columnChartOptions: Highcharts.Options = {
     chart: {
+      width: 900,
+      backgroundColor: 'rgba(242, 242, 242, 0.5)',
       type: 'column'
     },
     title: {
-      text: 'Sales Data'
+      text: 'Total containers Arrivel'
     },
     xAxis: {
       categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
@@ -28,20 +34,87 @@ export class DashboardComponent {
     },
     yAxis: {
       title: {
-        text: 'Sales'
+        text: 'Quantity of Containers'
       }
     },
     series: [
       {
-        type: 'column', // Adicione esta propriedade
+        type: 'column',
         name: 'Product A',
-        data: [100, 200, 300, 400, 500, 600]
+        data: [100, 200, 300, 400, 500, 600],
+        pointWidth: 60, // Aumenta a largura das colunas para 40 pixels
       },
       {
-        type: 'column', // Adicione esta propriedade
+        type: 'column',
         name: 'Product B',
-        data: [200, 300, 400, 500, 600, 700]
+        data: [200, 300, 400, 500, 600, 700],
+        pointWidth: 50, // Aumenta a largura das colunas para 40 pixels
       }
     ]
   };
+  pieChartOptions: Highcharts.Options = {
+    chart: {
+      width: 900,
+      backgroundColor: 'rgba(242, 242, 242, 0.5)',
+      type: 'pie'
+    },
+    title: {
+      text: 'Share of Container Costs'
+    },
+    plotOptions: {
+      pie: {
+        innerSize: '30%',
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.y}'
+        }
+      }
+    },
+    series: [
+      {
+        type: 'pie',
+        name: 'Sales',
+        data: [
+          { name: 'Product A', y: 2100 },
+          { name: 'Product B', y: 2500 }
+        ]
+      }
+    ]
+  };
+  pieChart2Options: Highcharts.Options = {
+    chart: {
+      width: 900,
+      backgroundColor: 'rgba(242, 242, 242, 0.5)',
+      type: 'pie'
+    },
+    title: {
+      text: 'Share of Container Costs'
+    },
+    plotOptions: {
+      pie: {
+        innerSize: '30%',
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.y}'
+        }
+      }
+    },
+    series: [
+      {
+        type: 'pie',
+        name: 'Sales',
+        data: [
+          { name: 'Product A', y: 2100 },
+          { name: 'Product B', y: 2500 }
+        ]
+      }
+    ]
+  };
+  buscarIntervalo(){
+
+  }
 }
